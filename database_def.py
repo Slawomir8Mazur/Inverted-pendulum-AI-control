c2bef43545ca158682ac70209dee4a1d93fe15d5
Slav <slawomir8mazur@gmail.com>
2019-03-03 16:32:43 +0100

    index problem solved

--- database_def.py
+++ database_def.py
@@ -21,12 +21,7 @@ class Record:
             if isinstance(args[0], Record):
                 self.record = args[0]
         else:
-            self.record = pd.DataFrame(columns=['M_1', 'I_2', 'L',
-                                                'A_1', 'V_1', 'U_1',
-                                                'E_2', 'W_2', 'Fi_2',
-                                                'K', 'B'],
-                                       index=[1],
-                                       dtype=np.float32)
+            self.record = self.new_record()
 
         for key, value in kwargs.items():
             self.record[key] = value
@@ -34,21 +29,23 @@ class Record:
         self.stack_of_movement = self.new_movement_record()
         self.last_movement = self.new_movement_record()
 
-    def new_record(self):
+    @staticmethod
+    def new_record():
         return pd.DataFrame(columns=['M_1', 'I_2', 'L',
                                      'A_1', 'V_1', 'U_1',
                                      'E_2', 'W_2', 'Fi_2',
                                      'K', 'B'],
-                            index=[1],
+                            index=[0],
                             dtype=np.float32)
 
-    def new_movement_record(self):
+    @staticmethod
+    def new_movement_record():
         return pd.DataFrame(columns=['A_1', 'V_1', 'U_1',
                                      'E_2', 'W_2', 'Fi_2'],
                             index=[1],
                             dtype=np.float32)
 
-    def give_movement(self):
+    def give_movement_param(self):
         column_names = ['A_1', 'V_1', 'U_1', 'E_2', 'W_2', 'Fi_2']
         return self.record[column_names]
 
@@ -74,12 +71,15 @@ class Record:
             self.record[key] = input_table[pos]
 
     def update_stacks(self):
-        self.stack_of_movement = self.stack_of_movement.append(self.last_movement)
-        self.last_movement = self.record
+        self.stack_of_movement = self.stack_of_movement.append(self.last_movement, ignore_index=True)
+        self.update_last_movement()
+        #self.last_movement = self.record
+
+    def update_last_movement(self):
+        self.last_movement = self.last_movement.append(self.give_movement_param(), ignore_index=True)
 
     def single_move(self, force_table_record):
-        self.update_stacks()
-
+        self.update_last_movement()
         new_record = self.record.copy()
         force, dt = force_table_record
 
@@ -89,7 +89,7 @@ class Record:
               - self.record['L']*np.cos(self.record['Fi_2'])*force/2
 
         new_record['A_1'] = F_1 / new_record['M_1']
-        new_record['E_1'] = M_2 / new_record['I_2']
+        new_record['E_2'] = M_2 / new_record['I_2']
 
         new_record['V_1'] = self.record['V_1'] + new_record['A_1']/dt
         new_record['W_2'] = self.record['W_2'] + new_record['E_2'] / dt
@@ -99,12 +99,10 @@ class Record:
 
         self.record = new_record
         return new_record
-        ''' 
-        2.Should return nothing
-        3.Enhence last_movement - should be adding only variables of movement
-        '''
 
     def move(self, force_table, dt_min=0.02):
+        self.update_stacks()
+
         for F, t in force_table:
             if t > dt_min:
                 dt = int(t//dt_min)
@@ -113,12 +111,16 @@ class Record:
                 self.single_move((F, t%dt))
             else:
                 self.single_move((F, t))
-    #Test this function
+
+    def visealize(self, features, source, separately):
+        if features == 'all':
+            features = self.new_record().columns
+            pass
 
 
 r = Record()
 r.position_set(30)
-print(r.give_movement())
+print(r.give_movement_param())
 r.move([(0, 0.1)], dt_min=0.01)
-#print(r.record.to_string())
-print(r.give_movement())
+print(r.last_movement.to_string())
+print(r.stack_of_movement.to_string(), end='\n-----------------------')
